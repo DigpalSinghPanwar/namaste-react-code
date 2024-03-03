fdeb35555ef028d928ee0776dac0b12d0dc79884
DigpalSinghPanwar784 <singh.digpal784@gmail.com>
2024-03-03 15:05:20 +0530

    episode 3 laying the foundation.

--- App.js
+++ App.js
@@ -1,29 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "heading",
-//     xyz: "abc",
-//   },
-//   "Hello World from React!"
-// );
 
-// React.createElement(object)  => HTML(Browser Understands)
+// React.createElement =>  ReactElement(JS object)  => HTMLElement(render) Browser Understands
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React!");
 
-console.log(parent); // object
+// console.log(heading); // object
+
+// JSX(transpiled before it reaches the js) - Parcel - Babel
+
+// JSX(babel) => Babel transpiles it to React.createElement =>  ReactElement(JS object)  => HTMLElement(render) Browser Understands
+
+// const title = <h1 id="heading">Namaste React Using JSX</h1> // React Element
+
+//Functitonal Component Using Return
+// const Title = () => {
+//   return (
+//     <div id="TitleContainer">
+//       <h1 id="heading">Namaste React Using JSX return</h1>;
+//     </div>
+//   );
+// };
+
+// Funcitonal Component with Normal Function
+// function Title() {
+//   return <h1 id="heading">Namaste React Using JSX Normal Funciton </h1>;
+// }
+
+// Funcitonal Component with Normal Function
+// const Title = function () {
+//   return <h1 id="heading">Namaste React Using JSX Normal Funciton </h1>;
+// };
+
+// Functional Component Without Return
+const Title = () => <h1 id="heading">Namaste React Using JSX</h1>;
+
+const elem = <h1>hi this is normal element</h1>;
+
+const title = (
+  <div>
+    This is title heading
+    {elem}
+  </div>
+);
+
+//JSX sanitize all JS code written inside {} parenthesis and prevents XSS cross site scripting attacks.
+
+// Component Composition
+// 3 ways to call a component
+const HeadingComponent = () => (
+  <div id="Container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 id="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
+// console.log(<HeadingComponent />); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
+// root.render(title);
