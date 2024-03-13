2d56b3a7a14833b198f45c2d288f4ce460b797d7
DigpalSinghPanwar784 <singh.digpal784@gmail.com>
2024-03-14 01:07:26 +0530

    episode 6 exploring the world.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,37 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData.js";
+import Shimmer from "./Shimmer.js";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-  // 2) useState
-  // const arr = useState(resList);
-  // const [listOfRestaurants, setListOfRestaurant] = arr;
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // 3)  useState
-  // const arr = useState(resList);
-  // const listOfRestaurants = arr[0];
-  // const setListOfRestaurant = arr[1];
+  // Whenever state variables update, react triggersa reconciliation cycle(re-renders the component)
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  // Conditional Rendering
+  if (filteredRestaurant.length === 0) {
+    return <Shimmer />;
+    // return <h1>Loading....</h1>;
+  }
 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            className="search-box"
+            value={searchText}
+          />
+          <button
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.4
+            const filteredList = filteredRestaurant.filter(
+              (res) => res.info.avgRating >= 4.4
             );
-            setListOfRestaurant(filteredList);
+            console.log(filteredList);
+            setFilteredRestaurant(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 export default Header = () => {
+  const [btnReact, setbtnReact] = useState("login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +14,16 @@ export default Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnReact === "login"
+                ? setbtnReact("logout")
+                : setbtnReact("login");
+            }}
+          >
+            {btnReact}
+          </button>
         </ul>
       </div>
     </div>
